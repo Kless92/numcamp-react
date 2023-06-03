c04d496aef44e891d925b64a76011e4316db83dd
Spencer Klessens <klessens92@gmail.com>
2023-06-03 12:11:10 -0500

    small change to Contact Screen using linebreak {n}

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -8,11 +8,8 @@ const ContactScreen = () => {
             <Card wrapperStyle={{ margin: 20 }}>
                 <Card.Title>Contact Infomation</Card.Title>
                 <Card.Divider/>
-                <Text>1 Nucamp Way</Text>
-                <Text>Seattle, WA 98001</Text>
-                <Text style={{marginBottom: 20}}>U.S.A</Text>
-                <Text>Phone: 1-206-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>
+                <Text style={{marginBottom: 20}}>1 Nucamp Way{"\n"}Seattle, WA 98001{"\n"}U.S.A</Text>
+                <Text>Phone: 1-206-555-1234{"\n"}Email: campsites@nucamp.co</Text>
             </Card>
         </ScrollView>
     )
