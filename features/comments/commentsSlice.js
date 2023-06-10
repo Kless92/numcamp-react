9c0ef61ce98152ef1b4f1b3d859f301d014d7d3d
Spencer Klessens <klessens92@gmail.com>
2023-06-10 14:14:11 -0500

    changes to RenderCampsite, commentSlice and CampsiteInfoScreen to submit a comment/rating, use stars have the new comment/rating on the page.

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../shared/baseUrl';
+import { DatePickerIOSBase } from 'react-native';
 
 export const fetchComments = createAsyncThunk(
     'comments/fetchComments',
@@ -15,27 +16,44 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, { dispatch, getState }) => {
+        setTimeout(() => {
+            const { comments } = getState();
+            payload.date = new Date().toISOString();
+            payload.id = comments.commentsArray.length;
+            dispatch(addComment(payload));
+        }, 2000);
+    }
+);
+
 const commentsSlice = createSlice({
     name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload);
+        }
+    },
     extraReducers: (builder) => {
         builder
-            .addCase(fetchComments.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(fetchComments.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.errMess = null;
-                state.commentsArray = action.payload;
-            })
-            .addCase(fetchComments.rejected, (state, action) => {
-                state.isLoading = false;
-                state.errMess = action.error
-                    ? action.error.message
-                    : 'Fetch failed';
-            });
+          .addCase(fetchComments.pending, (state) => {
+              state.isLoading = true;
+          })
+          .addCase(fetchComments.fulfilled, (state, action) => {
+              state.isLoading = false;
+              state.errMess = null;
+              state.commentsArray = action.payload;
+          })
+          .addCase(fetchComments.rejected, (state, action) => {
+              state.isLoading = false;
+              state.errMess = action.error
+                ? action.error.message
+                : 'Fetch failed';
+          });
     }
 });
 
+export const { addComment } = commentsSlice.actions;
 export const commentsReducer = commentsSlice.reducer;
